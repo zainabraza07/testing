67a45ee83252207098777a1f3749b0310a9a042d
Zainab Raza <razazainab13@gmail.com>
2026-02-02 20:35:09 +0500

    Update script.js

--- script.js
+++ script.js
@@ -127,14 +127,4 @@ navLinksAll.forEach(link => {
     window.scrollTo({ top: 0, behavior: "smooth" });
   });
 });
-const reveals = document.querySelectorAll('.reveal');
-
-window.addEventListener('scroll', () => {
-  reveals.forEach(el => {
-    const top = el.getBoundingClientRect().top;
-    if (top < window.innerHeight - 100) {
-      el.classList.add('active');
-    }
-  });
-});
 
